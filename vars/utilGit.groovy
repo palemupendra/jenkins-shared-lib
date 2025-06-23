d460bef45b3dcae5d5bffa0f5d115f1ea1d3dd62
palem upendra <palemupendra0209@gmail.com>
2025-06-23 14:05:34 +0530

    add

--- vars/utilGit.groovy
+++ vars/utilGit.groovy
@@ -1,11 +1,17 @@
 def setEnvVarsFromGitProperties() {
-    // You can customize this logic to fetch and set values
-    def gitCommit = sh(script: "git rev-parse HEAD", returnStdout: true).trim()
-    def gitBranch = sh(script: "git rev-parse --abbrev-ref HEAD", returnStdout: true).trim()
+    if (isUnix()) {
+        def commit = sh(script: "git rev-parse HEAD", returnStdout: true).trim()
+        def branch = sh(script: "git rev-parse --abbrev-ref HEAD", returnStdout: true).trim()
 
-    env.GIT_COMMIT_ID = gitCommit
-    env.GIT_BRANCH_NAME = gitBranch
+        env.GIT_COMMIT_ID = commit
+        env.GIT_BRANCH_NAME = branch
+    } else {
+        def commit = bat(script: "git rev-parse HEAD", returnStdout: true).trim()
+        def branch = bat(script: "git rev-parse --abbrev-ref HEAD", returnStdout: true).trim()
 
-    echo "Git Commit: ${env.GIT_COMMIT_ID}"
-    echo "Git Branch: ${env.GIT_BRANCH_NAME}"
+        env.GIT_COMMIT_ID = commit
+        env.GIT_BRANCH_NAME = branch
+    }
+
+    echo "Branch: ${env.GIT_BRANCH_NAME}, Commit: ${env.GIT_COMMIT_ID}"
 }
