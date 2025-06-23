1c126da0d8aed846222a6728fe35c7005889af67
palem upendra <palemupendra0209@gmail.com>
2025-06-23 19:42:19 +0530

    add

--- Jenkinsfile
+++ Jenkinsfile
@@ -17,5 +17,16 @@ pipeline {
                 echo "Commit: ${env.GIT_COMMIT_ID}"
             }
         }
+         stage('Sonar Analysis') {
+            steps {
+                runSonarAnalysis(
+                    projectKey: 'myproject',
+                    projectName: 'myproject',
+                    sonarHost: 'http://localhost:9000',
+                    sonarToken: 'sqp_cff03050c4b1df3e96eb1b931a6062df2de5f68d',       // Jenkins Secret Text credential ID
+                    sourceDir: '.'                       // Optional, default is '.'
+                )
+            }
+        }
     }
 }
---
+++ vars/runSonarAnalysis.groovy
@@ -0,0 +1,24 @@
+def call(Map config = [:]) {
+    def projectKey = config.projectKey ?: error("projectKey is required")
+    def projectName = config.projectName ?: projectKey
+    def sonarHost = config.sonarHost ?: 'http://localhost:9000'
+    def sonarToken = config.sonarToken ?: error("sonarToken (secret text credential ID) is required")
+    def sourceDir = config.sourceDir ?: '.'
+
+    stage('SonarQube Analysis') {
+        withCredentials([string(credentialsId: sonarToken, variable: 'SONAR_TOKEN')]) {
+            withEnv([
+                "SONAR_SCANNER_OPTS=-Djava.io.tmpdir=${env.WORKSPACE}/.sonar-temp"
+            ]) {
+                bat """
+                    sonar-scanner.bat ^
+                        -Dsonar.projectKey=${projectKey} ^
+                        -Dsonar.projectName=${projectName} ^
+                        -Dsonar.sources=${sourceDir} ^
+                        -Dsonar.host.url=${sonarHost} ^
+                        -Dsonar.login=%SONAR_TOKEN%
+                """
+            }
+        }
+    }
+}
